39ebbe08fdb1ad7d8d7bbde0c8df4e8d30191c6f
Cholito04 <mauriciomancerab@gmail.com>
2026-03-04 10:08:38 -0500

    fixed mobile3

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -6,18 +6,18 @@ function Hero() {
     <div className="text-cyan-200 w-full lg:px-4 px-10 py-36 mx-auto text-center">
       <div className=" mx-auto lg:max-w-310 max-w-200 p-3 text-center">
         <div
-          className={` mx-auto lg:max-w-180 max-w-150 rounded-t-xl w-full h-15 ${styles.playertop}`}
+          className={` mx-auto lg:max-w-180 max-w-120 rounded-t-xl w-full h-15 ${styles.playertop}`}
         ></div>
         <div className="flex justify-center flex-col w-full gap-4">
           <div
-            className={` p-14 mx-auto lg:max-w-180 max-w-150 rounded-b-3xl w-full ${styles.tv}`}
+            className={` md:p-18 p-10 mx-auto lg:max-w-180 max-w-120 rounded-b-3xl w-full ${styles.tv}`}
           >
-            <div className={`p-8 mx-auto rounded-3xl w-full ${styles.lcd}`}>
-              <h1 className=" m-5 xl:text-6xl text-3xl lg:max-w-310 max-w-200">
+            <div className={`md:p-10 p-4 mx-auto rounded-3xl w-full ${styles.lcd}`}>
+              <h1 className=" m-2 xl:text-6xl text-2xl lg:max-w-310 max-w-200">
                 {" "}
                 Music Reccomender
               </h1>
-              <p className="lg:text-xl text-sm m-5">
+              <p className="lg:text-xl text-sm">
                 {" "}
                 Recommend songs based of your public playlist or any public
                 playlist.
